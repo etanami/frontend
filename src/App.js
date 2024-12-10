5a1f478bc07de1b42cdee4ab4abea687c0ed8e92
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-12-10 18:53:07 +0100

    add: stripe frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import CheckoutForm from './CheckoutForm';
+import CompletePage from './CompletePage';
 import './App.css';
 
-function App() {
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  'pk_test_51QMYD5H00jC7Ty5A1mWcoCy54ex7BDwoHuoE3kHBdOPsarf6lhxgltAmfT2LZz8Fjh4zR1s6gmiu4L2XPo2L2u7t002J5aImqy'
+);
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState('');
+  const [dpmCheckerLink, setDpmCheckerLink] = useState('');
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch('/payments/create-payment-intent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ amount: 4999, currency: 'usd'}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setClientSecret(data.clientSecret);
+        // [DEV] For demo purposes only
+        setDpmCheckerLink(data.dpmCheckerLink);
+      });
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  // Enable the skeleton loader UI for optimal loading.
+  const loader = 'auto';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        {clientSecret && (
+          <Elements
+            options={{ clientSecret, appearance, loader }}
+            stripe={stripePromise}
+          >
+            <Routes>
+              <Route
+                path='/checkout'
+                element={<CheckoutForm dpmCheckerLink={dpmCheckerLink} />}
+              />
+              <Route path='/complete' element={<CompletePage />} />
+            </Routes>
+          </Elements>
+        )}
+      </div>
+    </Router>
   );
 }
-
-export default App;
